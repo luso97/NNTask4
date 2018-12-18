e49e57621bfbd948494f0dc36dd7604eab1d3519
Luis <Luis@LAPTOP-FGP77KQT>
2018-12-18 11:53:13 +0100

    El código está en p.py, si ves el latex verás que la parte de las 9 capas ocultas ya está hecha, lo que queda es lo mismo pero cn el apratado 2

--- source/programHL.py
+++ source/programHL.py
@@ -29,22 +29,23 @@ def normalizeDataNN(array):
     for i in range (len(m)):
         maximum=max(m[i]);
         minimum=min(m[i]);
+        avg=np.average(m[i]);
+        std=np.std(m[i]);
         for j in range(len(m[i])):
-            res[i][j]=float(m[i][j]-minimum)/float(maximum-minimum);
+            res[i][j]=float(m[i][j]-avg)/std;
     return transpose(res);
 
 Ca=createC(C);
 Ca_Test=createC(C_test);
 Xa=normalizeDataNN(Xa);
-Ca=normalizeDataNN(Ca);
+
 X_test=normalizeDataNN(X_test);
-Ca_Test=normalizeDataNN(Ca_Test);
+
 
 # Give the dimension of the data and chose the number of hidden layer
 
 n_in = 300
 n_hidden = 40
-n_hidden = 75
 n_hidden3 = 5
 n_out = 26
 x = tf.placeholder(shape=(None,300),dtype=tf.float64)
@@ -55,14 +56,14 @@ y1=tf.nn.relu(tf.matmul(x,W_hid) + b_hid);
 # Lets try adding another hidden layer (if we want it, decomment the line)
 W_hid2 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid2 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y2=tf.nn.relu(tf.matmul(y1,W_hid2)+b_hid2);
+y2=tf.nn.relu(tf.matmul(y1,W_hid2)+b_hid2); 
 # And another one (if we want it, decomment the line)
 W_hid3 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid3 = tf.Variable(np.zeros(n_hidden),trainable=True)
 y3=tf.nn.relu(tf.matmul(y2,W_hid3)+b_hid3);
 
 #first resudial layer
-yRes1=tf.nn.relu(y1+y3);
+yRes1=tf.nn.relu(y1+tf.matmul(y2,W_hid3)+b_hid3);
 
 W_hid4 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid4 = tf.Variable(np.zeros(n_hidden),trainable=True)
@@ -72,7 +73,7 @@ W_hid5 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True)
 b_hid5 = tf.Variable(np.zeros(n_hidden),trainable=True)
 y5=tf.nn.relu(tf.matmul(y4,W_hid5)+b_hid5);
 
-yRes2=tf.nn.relu(yRes1+y5);
+yRes2=tf.nn.relu(yRes1+tf.matmul(y4,W_hid5)+b_hid5);
 
 W_hid6 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid6 = tf.Variable(np.zeros(n_hidden),trainable=True)
@@ -82,7 +83,7 @@ W_hid7 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True)
 b_hid7 = tf.Variable(np.zeros(n_hidden),trainable=True)
 y7=tf.nn.relu(tf.matmul(y6,W_hid7)+b_hid7);
 
-yRes3=tf.nn.relu(yRes2+y7);
+yRes3=tf.nn.relu(yRes2+tf.matmul(y6,W_hid7)+b_hid7);
 
 W_hid8 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid8 = tf.Variable(np.zeros(n_hidden),trainable=True)
@@ -92,7 +93,7 @@ W_hid9 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True)
 b_hid9 = tf.Variable(np.zeros(n_hidden),trainable=True)
 y9=tf.nn.relu(tf.matmul(y8,W_hid9)+b_hid9);
 
-yRes4=tf.nn.relu(yRes3+y9);
+yRes4=tf.nn.relu(yRes3+tf.matmul(y8,W_hid9)+b_hid9);
 ####################################### 
 ### For 1 hidden layer, decomment the 1st w_out, for 2 the 2nd and for 3 the 3rd (keep the other commented)
 w_out = tf.Variable(rd.randn(n_hidden,n_out) / np.sqrt(n_in),trainable=True)
@@ -117,105 +118,36 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(z_ * tf.log(tf.clip_by_value(z,1e-
 #cross_entropy = tf.reduce_mean(-tf.reduce_sum(z_ * tf.log(z), reduction_indices=[1]));
 
 
-train_step = tf.train.GradientDescentOptimizer(0.2).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer().minimize(cross_entropy);
 
 correct_prediction = tf.equal(tf.argmax(z,1), tf.argmax(z_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
 init = tf.global_variables_initializer() # Create an op that will
 sess = tf.Session()
 sess.run(init) # Set the value of the variables to their initialization value
-
-
-
-# Re init variables to start from scratch
-sess.run(init)
-
 # Create some list to monitor how error decreases
-test_loss_list = []
-train_loss_list = []
 
-test_acc_list = []
-train_acc_list = []
-
-# Create minibtaches to train faster
-k_batch = 40
-X_batch_list = np.array_split(Xa,k_batch)
-labels_batch_list = np.array_split(Ca,k_batch)
-initial=np.random.randint(0,6238-100);
-validation_set=Xa[initial:initial+100][:];
-Ca_validation_set=Ca[initial:initial+100][:]
-maxacc=0.0;
-trainacc=0.0;
-saver = tf.train.Saver()
-epochs=0;
-counter=0;
-patience=100;
-#Training with validation set
-for k in range(500):
-    
-    # Run gradient steps over each minibatch
-    for x_minibatch,labels_minibatch in zip(X_batch_list,labels_batch_list):
-        sess.run(train_step, feed_dict={x: x_minibatch, z_:labels_minibatch})
-    counter=counter+1;
-    
-    # Compute the errors over the whole dataset
-    train_loss = sess.run(cross_entropy, feed_dict={x:Xa, z_:Ca})
-    test_loss = sess.run(cross_entropy, feed_dict={x:validation_set,z_:Ca_validation_set});
-    
-    # Compute the acc over the whole dataset
-    train_acc = sess.run(accuracy, feed_dict={x:Xa, z_:Ca})
-    test_acc = sess.run(accuracy, feed_dict={x:validation_set,z_:Ca_validation_set});
-    if maxacc<test_acc:
-        save_path = saver.save(sess, "/tmp/model.ckpt")
-        maxacc=test_acc;
-        trainacc=train_acc;
-        epochs=k;
-        
-        #Save state
-    
-    if counter>patience:
-        print("early stopping");
-        break;
-    # Put it into the lists
-    test_loss_list.append(test_loss)
-    train_loss_list.append(train_loss)
-    test_acc_list.append(test_acc)
-    train_acc_list.append(train_acc)
-    if k>0 and (test_acc_list[k]-test_acc_list[k-1])<0.01:
-        counter+=1;
-    else:
-        counter=0;
-    
-    if np.mod(k,100) == 0:
-        print('iteration {} test accuracy: {:.3f}'.format(k+1,test_acc))
-print('best val accuracy: '+format(maxacc));
-print('best train accuracy: '+format(trainacc));
-fig,ax_list = plt.subplots(1,2)
-ax_list[0].plot(train_loss_list, color='blue', label='training', lw=2)
-ax_list[0].plot(test_loss_list, color='green', label='testing', lw=2)
-ax_list[1].plot(train_acc_list, color='blue', label='training', lw=2)
-ax_list[1].plot(test_acc_list, color='green', label='testing', lw=2)
-
-ax_list[0].set_xlabel('training iterations')
-ax_list[1].set_xlabel('training iterations')
-ax_list[0].set_ylabel('Cross-entropy')
-ax_list[1].set_ylabel('Accuracy')
-plt.legend(loc=2)
-plt.show();
 #Now we use the test set
 test_loss_list2 = []
 train_loss_list2 = []
 
 test_acc_list2 = []
 train_acc_list2 = []
+saver = tf.train.Saver()
 
-saver.restore(sess, "/tmp/model.ckpt")
-saver2 = tf.train.Saver()
-print("epochs"+str(epochs));
+
+
 counter=0;
 patience=1000;
 maxacc=0;
+k_batch = 20
+X_batch_list = np.array_split(Xa,k_batch)
+labels_batch_list = np.array_split(Ca,k_batch)
 #retraining of the network with the epoch that suits best
+#adamAlgroithm
+
+
+epochs=100;
 for k in range(epochs):
     
     # Run gradient steps over each minibatch
@@ -225,11 +157,14 @@ for k in range(epochs):
     
     # Compute the errors over the whole dataset
     train_loss = sess.run(cross_entropy, feed_dict={x:Xa, z_:Ca})
-    test_loss = sess.run(cross_entropy, feed_dict={x:validation_set,z_:Ca_validation_set});
+    test_loss = sess.run(cross_entropy, feed_dict={x:X_test,z_:Ca_Test});
     
     # Compute the acc over the whole dataset
     train_acc = sess.run(accuracy, feed_dict={x:Xa, z_:Ca})
     test_acc = sess.run(accuracy, feed_dict={x:X_test,z_:Ca_Test});
+    
+    #computing ggradient
+    
     if maxacc<test_acc:
         maxacc=test_acc;
         save_path = saver.save(sess, "/tmp/model2.ckpt")
@@ -239,7 +174,7 @@ for k in range(epochs):
     train_loss_list2.append(train_loss)
     test_acc_list2.append(test_acc)
     train_acc_list2.append(train_acc)
-    if k>0 and (test_acc_list2[k]-test_acc_list2[k-1])<0.02:
+    if k>0 and (test_loss_list2[k]-test_loss_list2[k-1])<0.02:
         counter+=1;
     else:
         counter=0;
--- source/programHL2.py
+++ source/programHL2.py
@@ -29,61 +29,60 @@ def normalizeDataNN(array):
     for i in range (len(m)):
         maximum=max(m[i]);
         minimum=min(m[i]);
+        avg=np.average(m[i]);
+        std=np.std(m[i]);
         for j in range(len(m[i])):
-            res[i][j]=float(m[i][j]-minimum)/float(maximum-minimum);
+            res[i][j]=float(m[i][j]-avg)/std;
     return transpose(res);
-
 Ca=createC(C);
 Ca_Test=createC(C_test);
 Xa=normalizeDataNN(Xa);
-Ca=normalizeDataNN(Ca);
 X_test=normalizeDataNN(X_test);
-Ca_Test=normalizeDataNN(Ca_Test);
+
 
 # Give the dimension of the data and chose the number of hidden layer
 
 n_in = 300
 n_hidden = 40
-n_hidden = 75
 n_hidden3 = 5
 n_out = 26
 x = tf.placeholder(shape=(None,300),dtype=tf.float64)
 # Set the variables
 W_hid = tf.Variable(rd.randn(n_in,n_hidden) / np.sqrt(n_in),trainable=True)
 b_hid = tf.Variable(np.zeros(n_hidden),trainable=True);
-y1=tf.nn.relu(tf.matmul(x,W_hid) + b_hid);
+y1=tf.nn.tanh(tf.matmul(x,W_hid) + b_hid);
 # Lets try adding another hidden layer (if we want it, decomment the line)
 W_hid2 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid2 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y2=tf.nn.relu(tf.matmul(y1,W_hid2)+b_hid2);
+y2=tf.nn.tanh(tf.matmul(y1,W_hid2)+b_hid2);
 # And another one (if we want it, decomment the line)
 W_hid3 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid3 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y3=tf.nn.relu(tf.matmul(y2,W_hid3)+b_hid3);
+y3=tf.nn.tanh(tf.matmul(y2,W_hid3)+b_hid3);
 
 W_hid4 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid4 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y4=tf.nn.relu(tf.matmul(y3,W_hid4)+b_hid4);
+y4=tf.nn.tanh(tf.matmul(y3,W_hid4)+b_hid4);
 
 W_hid5 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid5 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y5=tf.nn.relu(tf.matmul(y4,W_hid5)+b_hid5);
+y5=tf.nn.tanh(tf.matmul(y4,W_hid5)+b_hid5);
 
 W_hid6 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid6 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y6=tf.nn.relu(tf.matmul(y5,W_hid6)+b_hid6);
+y6=tf.nn.tanh(tf.matmul(y5,W_hid6)+b_hid6);
 
 W_hid7 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid7 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y7=tf.nn.relu(tf.matmul(y6,W_hid7)+b_hid7);
+y7=tf.nn.tanh(tf.matmul(y6,W_hid7)+b_hid7);
 
 W_hid8 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid8 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y8=tf.nn.relu(tf.matmul(y7,W_hid8)+b_hid8);
+y8=tf.nn.tanh(tf.matmul(y7,W_hid8)+b_hid8);
 
 W_hid9 = tf.Variable(rd.randn(n_hidden,n_hidden) / np.sqrt(n_in),trainable=True);
 b_hid9 = tf.Variable(np.zeros(n_hidden),trainable=True)
-y9=tf.nn.relu(tf.matmul(y8,W_hid9)+b_hid9);
+y9=tf.nn.tanh(tf.matmul(y8,W_hid9)+b_hid9);
 ####################################### 
 ### For 1 hidden layer, decomment the 1st w_out, for 2 the 2nd and for 3 the 3rd (keep the other commented)
 w_out = tf.Variable(rd.randn(n_hidden,n_out) / np.sqrt(n_in),trainable=True)
@@ -108,105 +107,37 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(z_ * tf.log(tf.clip_by_value(z,1e-
 #cross_entropy = tf.reduce_mean(-tf.reduce_sum(z_ * tf.log(z), reduction_indices=[1]));
 
 
-train_step = tf.train.GradientDescentOptimizer(0.2).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=0.005).minimize(cross_entropy);
 
 correct_prediction = tf.equal(tf.argmax(z,1), tf.argmax(z_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
 init = tf.global_variables_initializer() # Create an op that will
 sess = tf.Session()
 sess.run(init) # Set the value of the variables to their initialization value
-
-
-
-# Re init variables to start from scratch
-sess.run(init)
-
 # Create some list to monitor how error decreases
-test_loss_list = []
-train_loss_list = []
 
-test_acc_list = []
-train_acc_list = []
-
-# Create minibtaches to train faster
-k_batch = 40
-X_batch_list = np.array_split(Xa,k_batch)
-labels_batch_list = np.array_split(Ca,k_batch)
-initial=np.random.randint(0,6238-100);
-validation_set=Xa[initial:initial+100][:];
-Ca_validation_set=Ca[initial:initial+100][:]
-maxacc=0.0;
-trainacc=0.0;
-saver = tf.train.Saver()
-epochs=0;
-counter=0;
-patience=100;
-#Training with validation set
-for k in range(500):
-    
-    # Run gradient steps over each minibatch
-    for x_minibatch,labels_minibatch in zip(X_batch_list,labels_batch_list):
-        sess.run(train_step, feed_dict={x: x_minibatch, z_:labels_minibatch})
-    counter=counter+1;
-    
-    # Compute the errors over the whole dataset
-    train_loss = sess.run(cross_entropy, feed_dict={x:Xa, z_:Ca})
-    test_loss = sess.run(cross_entropy, feed_dict={x:validation_set,z_:Ca_validation_set});
-    
-    # Compute the acc over the whole dataset
-    train_acc = sess.run(accuracy, feed_dict={x:Xa, z_:Ca})
-    test_acc = sess.run(accuracy, feed_dict={x:validation_set,z_:Ca_validation_set});
-    if maxacc<test_acc:
-        save_path = saver.save(sess, "/tmp/model.ckpt")
-        maxacc=test_acc;
-        trainacc=train_acc;
-        epochs=k;
-        
-        #Save state
-    
-    if counter>patience:
-        print("early stopping");
-        break;
-    # Put it into the lists
-    test_loss_list.append(test_loss)
-    train_loss_list.append(train_loss)
-    test_acc_list.append(test_acc)
-    train_acc_list.append(train_acc)
-    if k>0 and (test_acc_list[k]-test_acc_list[k-1])<0.01:
-        counter+=1;
-    else:
-        counter=0;
-    
-    if np.mod(k,100) == 0:
-        print('iteration {} test accuracy: {:.3f}'.format(k+1,test_acc))
-print('best val accuracy: '+format(maxacc));
-print('best train accuracy: '+format(trainacc));
-fig,ax_list = plt.subplots(1,2)
-ax_list[0].plot(train_loss_list, color='blue', label='training', lw=2)
-ax_list[0].plot(test_loss_list, color='green', label='testing', lw=2)
-ax_list[1].plot(train_acc_list, color='blue', label='training', lw=2)
-ax_list[1].plot(test_acc_list, color='green', label='testing', lw=2)
-
-ax_list[0].set_xlabel('training iterations')
-ax_list[1].set_xlabel('training iterations')
-ax_list[0].set_ylabel('Cross-entropy')
-ax_list[1].set_ylabel('Accuracy')
-plt.legend(loc=2)
-plt.show();
 #Now we use the test set
 test_loss_list2 = []
 train_loss_list2 = []
 
 test_acc_list2 = []
 train_acc_list2 = []
+saver = tf.train.Saver()
 
-saver.restore(sess, "/tmp/model.ckpt")
-saver2 = tf.train.Saver()
-print("epochs"+str(epochs));
+
+
 counter=0;
-patience=1000;
+patience=100;
 maxacc=0;
+k_batch = 20
+X_batch_list = np.array_split(Xa,k_batch)
+labels_batch_list = np.array_split(Ca,k_batch)
 #retraining of the network with the epoch that suits best
+#adamAlgroithm
+trainMaxAcc=0.0
+testMinLoss=500.0
+epochs=250;
+earlyStop=0;
 for k in range(epochs):
     
     # Run gradient steps over each minibatch
@@ -216,12 +147,18 @@ for k in range(epochs):
     
     # Compute the errors over the whole dataset
     train_loss = sess.run(cross_entropy, feed_dict={x:Xa, z_:Ca})
-    test_loss = sess.run(cross_entropy, feed_dict={x:validation_set,z_:Ca_validation_set});
+    test_loss = sess.run(cross_entropy, feed_dict={x:X_test,z_:Ca_Test});
     
     # Compute the acc over the whole dataset
     train_acc = sess.run(accuracy, feed_dict={x:Xa, z_:Ca})
     test_acc = sess.run(accuracy, feed_dict={x:X_test,z_:Ca_Test});
-    if maxacc<test_acc:
+    if testMinLoss>test_loss and earlyStop==0:
+        testMinLoss=test_loss;
+    #computing ggradient
+    if trainMaxAcc<train_acc and earlyStop==0:
+        trainMaxAcc=train_acc;
+    
+    if maxacc<test_acc and earlyStop==0:
         maxacc=test_acc;
         save_path = saver.save(sess, "/tmp/model2.ckpt")
         #Save state 
@@ -230,13 +167,13 @@ for k in range(epochs):
     train_loss_list2.append(train_loss)
     test_acc_list2.append(test_acc)
     train_acc_list2.append(train_acc)
-    if k>0 and (test_acc_list2[k]-test_acc_list2[k-1])<0.02:
+    if k>0 and (test_loss_list2[k]-trainMaxAcc)>0.02:
         counter+=1;
     else:
         counter=0;
     if counter>patience:
         print("early stopping");
-        break;
+        earlyStop=1;
     
     if np.mod(k,100) == 0:
         print('iteration {} test accuracy: {:.3f}'.format(k+1,test_acc))
@@ -254,3 +191,4 @@ plt.legend(loc=2)
 
 plt.show();
 print('best test set accuracy: {:.3f}'.format(maxacc));
+print('best test set accuracy: {:.3f}'.format(trainMaxAcc));
